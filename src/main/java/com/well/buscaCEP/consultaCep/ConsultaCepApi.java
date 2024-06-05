69783de75128f2d37041f77366aab3f17e466d0c
Wsanbey <welrysanbey@hotmail.com>
2024-06-05 14:30:55 -0300

    front atualizado

--- src/main/java/com/well/buscaCEP/consultaCep/ConsultaCepApi.java
+++ src/main/java/com/well/buscaCEP/consultaCep/ConsultaCepApi.java
@@ -2,22 +2,27 @@ package com.well.buscaCEP.consultaCep;
 
 import com.well.buscaCEP.DTO.ConsultaCepDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
+@Controller
 @RequestMapping("/consulta-cep")
 public class ConsultaCepApi {
 
-    @GetMapping("{cep}")
-    public ConsultaCepDTO buscarCep(@PathVariable("cep") String cep){
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
 
+    @PostMapping("/buscar")
+    public String buscarCep(@RequestParam("cep") String cep, Model model) {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<ConsultaCepDTO> resp = restTemplate.getForEntity(String.format("https://viacep.com.br/ws/%s/json/",cep),ConsultaCepDTO.class);
+        ResponseEntity<ConsultaCepDTO> resp = restTemplate.getForEntity(
+                String.format("https://viacep.com.br/ws/%s/json/", cep), ConsultaCepDTO.class);
 
-        return resp.getBody();
+        model.addAttribute("infCEP", resp.getBody());
+        return "home";
     }
 }
